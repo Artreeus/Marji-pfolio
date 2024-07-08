e776f78ecba1ab692ade579a671401727380111d
Artreeus <142108262+Artreeus@users.noreply.github.com>
2024-07-08 20:35:57 +0600

    about me fixed

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -13,25 +13,18 @@ const AboutMe = () => {
         <p className="text-xl md:text-3xl font-medium md:leading-relaxed text-textColor">
           I'm{" "}
           <span className="font-bold tracking-wider text-white">
-            Sarah Jonson
+          Mehejerin Marji 
           </span>{" "}
           and I'm a freelance{" "}
-          <span className="font-bold text-white">Web Designer</span>. Lorem
-          Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry's standard dummy text ever since the
-          1500s, when an unknown printer took a galley of type and scrambled it
-          to make a type specimen book.
+          <span className="font-bold text-white">Web Designer</span>.I studied from Dhaka Polytechnic Institute. Now i am doing interny on ui/ux design from People and tech academy Dhaka,Bangladesh.
         </p>
         <div className="lg:w-[700px] grid grid-cols-1 md:grid-cols-2 gap-8">
           <AboutList title="Web design" />
           <AboutList title="Graphic & print" />
           <AboutList title="iOS design" />
-          <AboutList title="Front-end development" />
           <AboutList title="User experience" />
           <AboutList title="Branding" />
-          <AboutList title="Back-end development" />
           <AboutList title="Responsive" />
-          <AboutList title="Wordpress Website" />
         </div>
       </div>
     </section>
