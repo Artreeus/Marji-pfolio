ae44e00125d4f5d1c1cce7ed25a16eff754200ea
Artreeus <142108262+Artreeus@users.noreply.github.com>
2024-07-08 20:30:29 +0600

    bg image fixed

--- components/Design.tsx
+++ components/Design.tsx
@@ -2,7 +2,6 @@ import { useCallback } from "react";
 import type { Container, Engine } from "tsparticles-engine";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-import BannerImg from "../public/assets/bg.jpg";
 
 const Design = () => {
   const particlesInit = useCallback(async (engine: Engine) => {
@@ -21,7 +20,7 @@ const Design = () => {
     []
   );
   return (
-    <div className="w-full h-full bg-hero-image lg:bg-left bg-cover relative">
+    <div className="w-full lg:h-[700px] bg-hero-image bg-cover bg-center bg-no-repeat">
       <Particles
         className="w-full h-full"
         id="tsparticles"
